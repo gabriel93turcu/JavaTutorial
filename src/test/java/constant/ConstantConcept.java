ae64b236038eaed7022735e260f803aaed8bcd05
Gabriel Turcu <turcu.gabriel93@gmail.com>
2025-03-06 19:20:25 +0200

    Constant concept.

---
+++ src/test/java/constant/ConstantConcept.java
@@ -0,0 +1,19 @@
+package constant;
+
+import org.testng.annotations.Test;
+
+public class ConstantConcept {
+
+    //constantă = conceptul prin care definim o proprietate care nu își mai modifică valoarea inițială (ex: credențiale)
+
+    public final String nume = "Gabriel";
+
+    @Test
+    public void testMethod() {
+        System.out.println(nume);
+
+        //nume = "Bogdan";
+        System.out.println(nume);
+    }
+
+}
